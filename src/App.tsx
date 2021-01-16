5323a116152d532c85426d6cc3c5cab2965f0504
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-01-16 10:03:01 +0900

    add: comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { db } from './lib/firebase';
+import Post from './components/Post';
+export type UserType = {
+  id: string;
+  name: string;
+  state: string;
+};
 
 const App: React.FC<{}> = () => {
+  const [data, setData] = useState<UserType[]>([]);
+  useEffect(() => {
+    const unSub = db.collection('posts').onSnapshot(function (querySnapshot) {
+      setData(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+          state: doc.data().state,
+        }))
+      );
+    });
+    return () => {
+      unSub();
+    };
+  }, []);
+
   const handleClick = () => {
     // Add a new document in collection "cities"
-    db.collection('cities')
-      .doc('LA')
-      .set({
+    db.collection('posts')
+      .add({
         name: 'Los Angeles',
         state: 'CA',
         country: 'USA',
@@ -20,9 +41,21 @@ const App: React.FC<{}> = () => {
       });
   };
   return (
-    <div className="App">
+    <div>
       <h1>button</h1>
       <button onClick={handleClick}>button</button>
+      <h1>data</h1>
+      {data &&
+        data.map((element, index) => {
+          return (
+            <Post
+              key={index}
+              id={element.id}
+              name={element.name}
+              state={element.state}
+            />
+          );
+        })}
     </div>
   );
 };
---
+++ src/components/Post.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import { UserType } from '../App';
+import { db } from '../lib/firebase';
+
+type CommentType = {
+  name: string;
+  age: number;
+  text: string;
+};
+
+const Post: React.FC<UserType> = ({ id, name, state }) => {
+  const [comments, setComments] = useState<CommentType[]>([]);
+  useEffect(() => {
+    const unSub = db
+      .collection('posts')
+      .doc(id)
+      .collection('comment')
+      .onSnapshot(function (querySnapshot) {
+        setComments(
+          querySnapshot.docs.map((doc) => ({
+            name: doc.data().name,
+            age: doc.data().age,
+            text: doc.data().text,
+          }))
+        );
+      });
+    return () => unSub();
+  }, [id]);
+  const childMake = () => {
+    // Add a new document in collection "cities"
+    db.collection('posts')
+      .doc(id)
+      .collection('comment')
+      .add({
+        name: 'Los',
+        age: 28,
+        text: 'Hoo',
+      })
+      .then(function () {
+        console.log('Document successfully written!');
+      })
+      .catch(function (error) {
+        console.error('Error writing document: ', error);
+      });
+  };
+  return (
+    <div>
+      <hr />
+      <ul>
+        <li>id : {id}</li>
+        <li>name : {name}</li>
+        <li>state : {state}</li>
+      </ul>
+      <h3>comments</h3>
+      {comments &&
+        comments.map((comment, index) => {
+          return (
+            <div key={index}>
+              <ul>
+                <li>{comment.name}</li>
+                <li>{comment.age}</li>
+                <li>{comment.text}</li>
+              </ul>
+            </div>
+          );
+        })}
+      <button onClick={childMake}>comment</button>
+      <hr />
+    </div>
+  );
+};
+
+export default Post;
