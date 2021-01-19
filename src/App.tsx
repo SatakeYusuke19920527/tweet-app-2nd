15d1af5ab38a90f70a45a3ae9a7711e21b125e8d
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-01-20 08:00:44 +0900

    file upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { db, storage } from './lib/firebase';
+import firebase from 'firebase/app';
 import Post from './components/Post';
+import { Avatar } from '@material-ui/core';
 export type UserType = {
   id: string;
   name: string;
@@ -10,7 +12,9 @@ export type UserType = {
 
 const App: React.FC<{}> = () => {
   const [data, setData] = useState<UserType[]>([]);
+  const [data1, setData1] = useState<any>([]);
   const [item, setItem] = useState<File | null>(null);
+  const [isUpload, setIsUpload] = useState<boolean>(false);
   useEffect(() => {
     const unSub = db.collection('posts').onSnapshot(function (querySnapshot) {
       setData(
@@ -18,6 +22,22 @@ const App: React.FC<{}> = () => {
           id: doc.id,
           name: doc.data().name,
           state: doc.data().state,
+        }))
+      );
+    });
+    return () => {
+      unSub();
+    };
+  }, []);
+
+  useEffect(() => {
+    const unSub = db.collection('images').onSnapshot(function (querySnapshot) {
+      setData1(
+        querySnapshot.docs.map((doc) => ({
+          name: doc.data().name,
+          age: doc.data().age,
+          url: doc.data().url,
+          createAt: doc.data().createAt,
         }))
       );
     });
@@ -46,15 +66,60 @@ const App: React.FC<{}> = () => {
     setItem(file[0]);
   };
   const uploadFile = async () => {
-    await storage.ref(`image`).child(item?.name!).put(item!);
+    const S = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const N = 16;
+    const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+      .map((n) => S[n % S.length])
+      .join('');
+    const fileName = randomChar + '_' + item?.name;
+
+    const uploadItem = storage.ref(`image`).child(fileName).put(item!);
+
+    uploadItem.on(
+      firebase.storage.TaskEvent.STATE_CHANGED,
+      () => {
+        setIsUpload(true);
+        console.log('please wait');
+      },
+      (err: any) => {
+        console.log(err.messsage);
+      },
+      async () => {
+        setIsUpload(false);
+        console.log('file upload success!!');
+        await storage
+          .ref('image')
+          .child(fileName)
+          .getDownloadURL()
+          .then(async (url) => {
+            await db.collection('images').add({
+              name: 'satake',
+              age: 28,
+              url: url,
+              createAt: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+          });
+      }
+    );
   };
+  console.log(data1, 'hello ');
   return (
     <div>
       <input type="file" onChange={(e) => handleInput(e.target.files)} />
       <button onClick={uploadFile}>file upload</button>
       <h1>button</h1>
+      {isUpload && <h1>Uploading...</h1>}
       <button onClick={handleClick}>button</button>
       <h1>data</h1>
+      {data1 &&
+        data1.map((element: any, index: any) => {
+          return (
+            <div key={index}>
+              {element.name} / {element.age}
+              <Avatar src={element.url} alt="user name" />
+            </div>
+          );
+        })}
       {data &&
         data.map((element, index) => {
           return (
