72a27c08a80081713ed2d18f012a41c783c6fb1f
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-01-21 21:57:51 +0900

    create: line notify

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,21 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LineNotify from './components/LineNotify';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route exact path="/line" component={LineNotify} />
+        </Switch>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
